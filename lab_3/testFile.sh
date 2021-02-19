b6acc936806eda89dc39b681c7533c5f2ff7ae07
mZarifa <matthewzarifa@gmail.com>
2021-02-19 16:30:57 -0700

    First commit
    Initial commit

---
+++ lab_3/testFile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script follows here:
+
+$input
+
+if [input = -d "sample_text.txt"]
+    echo "Choose an option:"
+    echo "1. Read lines"
+    echo "2. Count words"
+    echo "3. add_text"
+    echo "4. Copy the folder and exit the program"
+    read input
+if [input = 1]
